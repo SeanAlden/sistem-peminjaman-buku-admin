526714a2c684bdf08c589437c3e7f0a07c6e951a
Sean Alden <sa2596491@gmail.com>
2025-11-08 20:32:31 +0700

    Fix import at api.php

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoanController;
 use App\Http\Controllers\Api\ChatController;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\LoanController;
-use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\FavoriteController;
-use App\Http\Controllers\API\CategoryController;
-use App\Http\Controllers\API\BookDetailController;
-use App\Http\Controllers\API\ReservationController;
+use App\Http\Controllers\Api\CategoryController;
+// use App\Http\Controllers\Api\BookDetailController;
+use App\Http\Controllers\Api\ReservationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
