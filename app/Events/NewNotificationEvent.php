cc76325405e94c4c9b04d7fd65c78d8bd0afb181
Sean Alden <c14210052@john.petra.ac.id>
2026-03-22 21:24:39 +0700

    update header #2

--- app/Events/NewNotificationEvent.php
+++ app/Events/NewNotificationEvent.php
@@ -1,15 +1,49 @@
 <?php
 
+// namespace App\Events;
+
+// use App\Models\Notification;
+// use Illuminate\Broadcasting\Channel;
+// use Illuminate\Queue\SerializesModels;
+// use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+// class NewNotificationEvent implements ShouldBroadcast
+// {
+//     use SerializesModels;
+
+//     public $notification;
+
+//     public function __construct(Notification $notification)
+//     {
+//         $this->notification = $notification;
+//     }
+
+//     public function broadcastOn()
+//     {
+//         return new Channel('notifications');
+//     }
+
+//     public function broadcastAs()
+//     {
+//         return 'new-notification';
+//     }
+// }
+
 namespace App\Events;
 
 use App\Models\Notification;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+// UBAH 1: Gunakan ShouldBroadcastNow
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class NewNotificationEvent implements ShouldBroadcast
+// UBAH 2: Implement ShouldBroadcastNow
+class NewNotificationEvent implements ShouldBroadcastNow
 {
-    use SerializesModels;
+    // UBAH 3: Tambahkan Dispatchable dan InteractsWithSockets (Standar Laravel Event)
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $notification;
 
